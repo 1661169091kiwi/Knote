2ecfc03cb6962b5418f21c7ffa3683e8e6f456b1
1661169091kiwi <1661169091kiwi@users.noreply.github.com>
2026-08-23 15:38:20 +0000

    deploy: 76d0d9f37811f29680033d35bf122e6ed8e24bd8

--- assets/swimlanesDiagram-G3AALYLV-Dix8d5Yy.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-CpIOz-Bm.js";import{_ as a}from"./mermaid.core-CLXWwwm5.js";import"./chunk-5VM5RSS4-Ca5XhD9Y.js";import"./chunk-XXDRQBXY-BBMA9vUn.js";import"./chunk-VR4S4FIN-DjUVArYI.js";import"./chunk-32BRIVSS-Bs4BACsc.js";import"./index-B-qW7bMi.js";import"./channel-CWOQLMkS.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
